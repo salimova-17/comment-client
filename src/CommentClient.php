5592ea89507423f18c77748d0b92d8c0a814e6cf
Aliya Salimova <salimova2505@gmail.com>
2021-01-25 04:14:23 +0500

    code and test

--- src/CommentClient.php
+++ src/CommentClient.php
@@ -4,10 +4,93 @@ declare(strict_types=1);
 
 namespace App;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+
+/**
+ * Class CommentClient
+ * @package App
+ */
 class CommentClient
 {
-    public function __construct()
-    {
+    protected Client $httpClient;
 
+    public function __construct(Client $httpClient)
+    {
+        $this->httpClient = $httpClient;
+    }
+
+    protected function getBaseUri(): string
+    {
+        return 'https://example.com/api/v1';
+    }
+
+    /**
+     * @return mixed|null
+     * @throws \JsonException
+     */
+    public function getComments(): mixed
+    {
+        $uri = $this->getBaseUri() . '/comments';
+        try {
+            $response = $this->httpClient->get($uri);
+            if ($response->getStatusCode() !== 200 || !$response->getBody()->getSize()) {
+                return null;
+            }
+            return json_decode($response->getBody()->getContents(), true, 512, JSON_THROW_ON_ERROR);
+        } catch (GuzzleException $e) {
+            return null;
+        }
+    }
+
+    /**
+     * @param $name
+     * @param $text
+     * @return mixed|null
+     * @throws \JsonException
+     */
+    public function addComment($name, $text): mixed
+    {
+        $uri = $this->getBaseUri() . '/comment';
+        try {
+            $response = $this->httpClient->post($uri, [
+                'form_params' => [
+                    'name' => $name,
+                    'text' => $text,
+                ]
+            ]);
+            if ($response->getStatusCode() !== 200 || !$response->getBody()->getSize()) {
+                return null;
+            }
+            return json_decode($response->getBody()->getContents(), true, 512, JSON_THROW_ON_ERROR);
+        } catch (GuzzleException $e) {
+            return null;
+        }
+    }
+
+    /**
+     * @param $id
+     * @param $name
+     * @param $text
+     * @return mixed|null
+     * @throws \JsonException
+     */
+    public function editComment($id, $name, $text): mixed
+    {
+        $uri = $this->getBaseUri() . '/comment/' . $id;
+        try {
+            $response = $this->httpClient->put($uri, [
+                'form_params' => [
+                    'name' => $name,
+                    'text' => $text,
+                ]
+            ]);
+            if ($response->getStatusCode() !== 200 || !$response->getBody()->getSize()) {
+                return null;
+            }
+            return json_decode($response->getBody()->getContents(), true, 512, JSON_THROW_ON_ERROR);
+        } catch (GuzzleException $e) {
+            return null;
+        }
     }
 }
---
+++ tests/CommentClientTest.php
@@ -0,0 +1,122 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use App\CommentClient;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class CommentClientTest
+ * @package Tests
+ * @covers \App\CommentClient
+ */
+class CommentClientTest extends TestCase
+{
+    protected CommentClient $commentClient;
+    protected MockHandler $mockHandler;
+
+
+    protected function setUp(): void
+    {
+        $this->mockHandler = new MockHandler();
+
+        $httpClient = new Client([
+            'handler' => $this->mockHandler,
+        ]);
+
+        $this->commentClient = new CommentClient($httpClient);
+    }
+
+
+    public function testCommentsAreRetrieved(): void
+    {
+        $this->mockHandler->append(new Response(200, [], file_get_contents(__DIR__ . '/fixtures/comments.json')));
+
+        $comments = $this->commentClient->getComments();
+
+        self::assertIsArray($comments);
+        self::assertCount(3, $comments);
+    }
+
+    public function testCommentsAreEmptyWhenResponseError(): void
+    {
+        $this->mockHandler->append(new Response(404));
+
+        $comments = $this->commentClient->getComments();
+
+        self::assertEmpty($comments);
+    }
+
+    public function testCommentsAreEmptyWhenException(): void
+    {
+        $this->mockHandler->append(new RequestException('Error Communicating with Server', new Request('GET', 'test')));
+
+        $comments = $this->commentClient->getComments();
+
+        self::assertEmpty($comments);
+    }
+
+    public function testAddCommentSuccess(): void
+    {
+        $this->mockHandler->append(new Response(200, [], file_get_contents(__DIR__ . '/fixtures/add-comment.json')));
+
+        $comment = $this->commentClient->addComment('added-comment', 'added-comment-text');
+
+        self::assertSame('added-comment', $comment['name']);
+        self::assertSame('added-comment-text', $comment['text']);
+    }
+
+    public function testAddCommentResponseErrorHandled(): void
+    {
+        $this->mockHandler->append(new Response(500));
+
+        $comment = $this->commentClient->addComment('added-comment', 'added-comment-text');
+
+        self::assertEmpty($comment);
+    }
+
+    public function testAddCommentExceptionHandled(): void
+    {
+        $this->mockHandler->append(new RequestException('Error Communicating with Server', new Request('POST', 'test')));
+
+        $comment = $this->commentClient->addComment('added-comment', 'added-comment-text');
+
+        self::assertEmpty($comment);
+    }
+
+    public function testEditCommentSuccess(): void
+    {
+        $this->mockHandler->append(new Response(200, [], file_get_contents(__DIR__ . '/fixtures/edit-comment.json')));
+
+        $comment = $this->commentClient->editComment(1,'edited-comment', 'edited-comment-text');
+
+        self::assertSame('edited-comment', $comment['name']);
+        self::assertSame('edited-comment-text', $comment['text']);
+    }
+
+    public function testEditCommentResponseErrorHandled(): void
+    {
+        $this->mockHandler->append(new Response(500));
+
+        $comment = $this->commentClient->editComment(1,'edited-comment', 'edited-comment-text');
+
+        self::assertEmpty($comment);
+
+    }
+
+    public function testEditCommentExceptionHandled(): void
+    {
+        $this->mockHandler->append(new RequestException('Error Communicating with Server', new Request('POST', 'test')));
+
+        $comment = $this->commentClient->editComment(1,'edited-comment', 'edited-comment-text');
+
+        self::assertEmpty($comment);
+    }
+}
